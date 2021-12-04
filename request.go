8e9054460d1718c990b9f32ee231957045e6042b
Roger <cheny.roger@gmail.com>
2021-12-04 15:45:35 +0800

    0.0.2

--- request.go
+++ request.go
@@ -47,7 +47,25 @@ type Request struct {
 	statusCode           int
 }
 
-func Do(rawURL string, options ...Option) error {
+func POST(rawURL string, options ...Option) (*Request, error) {
+	options = append(options, WithMethodPost())
+	return Do(rawURL, options...)
+}
+
+func PUT(rawURL string, options ...Option) (*Request, error) {
+	options = append(options, WithMethodPut())
+	return Do(rawURL, options...)
+}
+
+func PATCH(rawURL string, options ...Option) (*Request, error) {
+	options = append(options, WithMethodPatch())
+	return Do(rawURL, options...)
+}
+func OPTIONS(rawURL string, options ...Option) (*Request, error) {
+	options = append(options, WithMethodOptions())
+	return Do(rawURL, options...)
+}
+func NewRequest(rawURL string, options ...Option) *Request {
 	r := &Request{config: NewConfig(), rawURL: rawURL, query: make(url.Values), method: http.MethodGet, header: make(http.Header)}
 	for _, option := range options {
 		option(r)
@@ -78,14 +96,48 @@ func Do(rawURL string, options ...Option) error {
 		}
 		r.client = &http.Client{Transport: transport}
 	}
-	return r.Do()
+	return r
 }
 
-func (r *Request) Response() *http.Response {
+func Do(rawURL string, options ...Option) (*Request, error) {
+	r := &Request{config: NewConfig(), rawURL: rawURL, query: make(url.Values), method: http.MethodGet, header: make(http.Header)}
+	for _, option := range options {
+		option(r)
+	}
+
+	if r.client == nil {
+		transport := &http.Transport{
+			DialContext: func(_ context.Context, network, addr string) (net.Conn, error) {
+				conn, err := net.DialTimeout(network, addr, r.config.HTTPTimeout.ConnectTimeout)
+				if err != nil {
+					return nil, err
+				}
+				return newTimeoutConn(conn, r.config.HTTPTimeout), nil
+			},
+			ResponseHeaderTimeout: r.config.HTTPTimeout.HeaderTimeout,
+			TLSClientConfig:       &tls.Config{InsecureSkipVerify: r.config.InsecureSkipVerify},
+		}
+
+		// Proxy
+		if r.config.UseProxy && r.config.ProxyHost != "" {
+			proxyURL, err := url.Parse(r.config.ProxyHost)
+			if err == nil {
+				if r.config.IsAuthProxy {
+					proxyURL.User = url.UserPassword(r.config.ProxyUser, r.config.ProxyPassword)
+				}
+				transport.Proxy = http.ProxyURL(proxyURL)
+			}
+		}
+		r.client = &http.Client{Transport: transport}
+	}
+	return r, r.send()
+}
+
+func (r *Request) GetResponse() *http.Response {
 	return r.response
 }
 
-func (r *Request) Request() *http.Request {
+func (r *Request) GetRequest() *http.Request {
 	return r.request
 }
 
@@ -93,7 +145,74 @@ func (r *Request) StatusCode() int {
 	return r.statusCode
 }
 
-func (r *Request) Do() (err error) {
+func (r *Request) Do() (statusCode int, err error) {
+	if len(r.errors) > 0 {
+		buf := new(bytes.Buffer)
+		for _, e := range r.errors {
+			buf.WriteString(e.Error())
+			buf.WriteString("\n")
+		}
+		err = errors.New(buf.String())
+		return
+	}
+	URL, err := url.Parse(r.rawURL)
+	if err != nil {
+		return
+	}
+	if r.query != nil {
+		URL.RawQuery = r.query.Encode()
+	}
+	var i uint8
+	for i = 0; i <= r.retryTimes; i++ {
+		r.request, err = http.NewRequestWithContext(context.Background(), r.method, URL.String(), r.requestBody)
+		if err != nil {
+			continue
+		}
+		r.request.Header = r.header
+
+		r.response, err = r.client.Do(r.request)
+		if err != nil {
+			continue
+		}
+		statusCode = r.response.StatusCode
+		break
+	}
+	if err != nil {
+		return
+	}
+	defer r.response.Body.Close()
+	if r.responseUnwrapTarget != nil || r.responseBodyWriteTo != nil {
+		if r.responseUnwrapTarget == nil && r.responseBodyWriteTo != nil {
+			_, err = io.Copy(r.responseBodyWriteTo, r.response.Body)
+			if err != nil {
+				return
+			}
+		} else if r.responseUnwrapTarget != nil && r.responseBodyWriteTo == nil {
+			err = json.NewDecoder(r.response.Body).Decode(r.responseUnwrapTarget)
+			if err != nil {
+				return
+			}
+		} else {
+			buf := new(bytes.Buffer)
+			_, err = io.Copy(buf, r.response.Body)
+			if err != nil {
+				return
+			}
+			err = json.Unmarshal(buf.Bytes(), r.responseUnwrapTarget)
+			if err != nil {
+				return
+			}
+			_, err = io.Copy(r.responseBodyWriteTo, buf)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	return
+}
+
+func (r *Request) send() (err error) {
 	if len(r.errors) > 0 {
 		buf := new(bytes.Buffer)
 		for _, e := range r.errors {
--- request_test.go
+++ request_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestSimple(t *testing.T) {
-	statusCode, err := NewRequest("http://example.com").Do()
+	statusCode, err := NewRequest("http://baidu.com").Do()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -14,11 +14,19 @@ func TestSimple(t *testing.T) {
 }
 
 func TestWithQuery(t *testing.T) {
-	r := NewRequest("http://example.com", WithQuery("key1", "value",""))
+	r := NewRequest("http://baidu.com", WithQuery("key1", "value", ""))
 	statusCode, err := r.Do()
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Logf("statusCode: %d", statusCode)
+	t.Logf("request path: %v", r.GetRequest().URL)
+}
+func TestDo(t *testing.T) {
+	r, err := Do("http://baidu.com", WithQuery("key1", "value", ""))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("statusCode: %d", r.StatusCode())
 	t.Logf("request path: %v", r.GetRequest().URL)
 }
